9d88a7ee4ad18d10f38c073c12947edddf4e21d7
Alok Sharma <sharmaalok02gwl@gmail.com>
2025-08-01 21:43:14 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -479,13 +479,13 @@ export const Bio = {
         {
           name: "Alok Sharma",
           img: "https://avatars.githubusercontent.com/u/139451198?s=400&u=b83a23378aa74f2ea19af8f91d52f4914c9d5e98&v=4",
-          linkedin: "https://www.linkedin.com/in/alok-sharma2002-/",
+          linkedin: "https://www.linkedin.com/in/alok-sharma2002/",
           github: "https://github.com/alok-2002/",
         },
         {
           name: "Sanskriti Yadav",
           img: "https://avatars.githubusercontent.com/u/139451838?s=400&u=1c4239c0f0ef6aae4d6a077d736a2da5bfdc4e01&v=4",
-          linkedin: "https://www.linkedin.com/in/Sanskriti-Yadav/",
+          linkedin: "https://www.linkedin.com/in/alok-sharma2002/",
           github: "https://github.com/sanskriti-yadav",
         },
       ],
